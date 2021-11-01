123b9336f59506d1ce9a804f598f13796d5f0462
mattf <maf7@sfu.ca>
2021-11-01 14:57:56 -0700

    Added ChildManager and CoinFlipResult classes
    Added ChildManager class to store the children and coin flips. Added the CoinFlipResult class to store information about the flip. Added methods to get desired information. Removed the Child class as only the name needs to be stored

--- app/src/main/java/cmpt276/phosphorus/childapp/model/ChildManager.java
+++ app/src/main/java/cmpt276/phosphorus/childapp/model/ChildManager.java
@@ -1,10 +1,29 @@
 package cmpt276.phosphorus.childapp.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChildManager {
-    private List<Child> children;
+    private List<String> children;
+    private List<CoinFlipResult> flips;
 
+    public ChildManager(){
+        this.children = new ArrayList<>();
+        this.flips = new ArrayList<>();
+    }
 
+    public void addChild(String name){
+        this.children.add(name);
+    }
+
+    public void addFlip(CoinFlipResult result){
+        this.flips.add(result);
+    }
+
+    public String getChild(int i){return this.children.get(i);}
+
+    public CoinFlipResult getFlip(int i){return this.flips.get(i);}
+
+    public String getFlipTime(int i){return getFlip(i).getFormattedTime();}
 }
---
+++ app/src/main/java/cmpt276/phosphorus/childapp/model/CoinFlipResult.java
@@ -0,0 +1,34 @@
+package cmpt276.phosphorus.childapp.model;
+
+import java.time.LocalDateTime;
+
+//Stored the result of a coin flip
+public class CoinFlipResult {
+    private String sidePicker;
+    private LocalDateTime time;
+    private boolean isHeads;
+    private boolean didWin;
+
+    public CoinFlipResult(String pick, boolean flip, boolean win){
+        this.sidePicker = pick;
+        this.time = LocalDateTime.now();
+        this.isHeads = flip;
+        this.didWin = win;
+    }
+
+    public String getSidePicker(){return this.sidePicker;}
+
+    public boolean isDidWin(){return this.didWin;}
+
+    public LocalDateTime getTime(){return this.time;}
+
+    //gets date/time in format YYYY/MM/DD HH:mm
+    public String getFormattedTime(){
+        String result = "" + time.getYear();
+        result += "/" + time.getMonth() + "/" + time.getDayOfYear();
+        result += " " + time.getHour() + ":" + time.getMinute();
+        return result;
+    }
+
+    public boolean isHeads(){return this.isHeads;}
+}
