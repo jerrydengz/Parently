8733f5d605d823adcd0206fc0cef9114a2180f35
Jack Clarke <jack@node-app.com>
2021-11-04 11:47:42 -0700

    Refactor CoinFlipIntent to be more clear on what coin sides are for

--- app/src/main/java/cmpt276/phosphorus/childapp/coinflip/utils/CoinFlipIntent.java
+++ app/src/main/java/cmpt276/phosphorus/childapp/coinflip/utils/CoinFlipIntent.java
@@ -3,6 +3,6 @@ package cmpt276.phosphorus.childapp.coinflip.utils;
 public class CoinFlipIntent {
 
     public static final String CHILD_UUID = "CHILD_UUID";
-    public static final String  COIN_SIDE = "COIN_SIDE";
+    public static final String CHOSEN_COIN_SIDE = "CHOSEN_COIN_SIDE";
 
 }
