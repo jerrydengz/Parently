70d82d349ea1bb55577534eea574d41b59bc55a2
Jerry <zrd1@sfu.ca>
2021-11-05 01:47:06 -0700

    Implement ListView of Child objects.
    Some pieces of code are in place for when I implement Editing

--- app/src/main/java/cmpt276/phosphorus/childapp/ChildrenActivity.java
+++ app/src/main/java/cmpt276/phosphorus/childapp/ChildrenActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ import cmpt276.phosphorus.childapp.model.ChildManager;
 public class ChildrenActivity extends AppCompatActivity {
 
     private ChildManager childManager;
+    private final int NEW_CHILD_IDX = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +26,52 @@ public class ChildrenActivity extends AppCompatActivity {
 
         childManager = ChildManager.getInstance();
         this.createBackBtn();
+        this.createConfigureChildBtn();
+        this.populateChildListView();
+        this.createOnClickCallBack();
     }
 
+    private void createConfigureChildBtn() {
+        Button button = findViewById(R.id.configureChildrenBtn);
+        button.setOnClickListener(view -> startActivity(ChildrenConfigureActivity.makeIntent(
+                this, null, NEW_CHILD_IDX, false)
+        ));
+    }
 
     private void createBackBtn(){
         Button button = findViewById(R.id.btnBackChildren);
         button.setOnClickListener(view -> finish());
+    }
+
+    private void populateChildListView() {
+        childManager.addChild(new Child("Jerry"));
+        childManager.addChild(new Child("Jack"));
+        childManager.addChild(new Child("Johnson"));
+        childManager.addChild(new Child("Joshua"));
+        childManager.addChild(new Child("Jonathan"));
+        childManager.addChild(new Child("Josh"));
+
+        List<Child> childProfileList = childManager.getAllChildren();
+        ArrayAdapter<Child> listAdapter = new ArrayAdapter<>(
+                this,
+                R.layout.child_profile,
+                childProfileList);
+
+        ListView listView = findViewById(R.id.listViewChildren);
+        listView.setAdapter(listAdapter);
+    }
+
+    private void createOnClickCallBack() {
+        ListView listView = findViewById(R.id.listViewChildren);
+
+        // switch to ChildConfigureActivity to edit selected child object
+        listView.setOnItemClickListener((adapter, view, position, arg) -> {
+            Intent intent = ChildrenConfigureActivity.makeIntent(
+                    getApplicationContext(),
+                    childManager.getAllChildren().get(position),
+                    position, true);
+            startActivity(intent);
+        });
     }
 
     public static Intent makeIntent(Context context) {
--- app/src/main/java/cmpt276/phosphorus/childapp/ChildrenConfigureActivity.java
+++ app/src/main/java/cmpt276/phosphorus/childapp/ChildrenConfigureActivity.java
@@ -1,16 +1,80 @@
 package cmpt276.phosphorus.childapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import cmpt276.phosphorus.childapp.model.Child;
+import cmpt276.phosphorus.childapp.model.ChildManager;
+
 public class ChildrenConfigureActivity extends AppCompatActivity {
+
+    private ChildManager childManager;
+    private boolean isEditingChild;
+    private static final String CONFIGURATION_STATE = "ChildConfigurationState";
+
+//    // TODO
+//    private final TextWatcher textWatcher = new TextWatcher() {
+//        @Override
+//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//        }
+//
+//        @Override
+//        public void onTextChanged(CharSequence s, int start, int before, int count) {
+//            // get the content of both the edit text
+//            String emailInput = etEmail.getText().toString();
+//            String passwordInput = etPassword.getText().toString();
+//
+//            // check whether both the fields are empty or not
+//            bLogin.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty());
+//        }
+//
+//        @Override
+//        public void afterTextChanged(Editable editable) {
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_children_configure);
+        childManager = ChildManager.getInstance();
+
+
+        this.createBackBtn();
+        this.createSaveBtn();
     }
 
+    private void createBackBtn(){
+        Button button = findViewById(R.id.btnBackChildrenConfigure);
+        button.setOnClickListener(view -> finish());
+    }
+
+    // TODO
+    private void createSaveBtn() {
+    }
+
+
+    public static Intent makeIntent(Context context, Child childObj, int position, boolean isEditing){
+        Intent intent = new Intent(context, ChildrenConfigureActivity.class);
+
+        intent.putExtra(CONFIGURATION_STATE, isEditing);
+
+        // TODO add intent extras for editing
+
+        return intent;
+    }
+
+    private void extractIntent() {
+        Intent packageInfo = getIntent();
+
+        isEditingChild = packageInfo.getBooleanExtra(CONFIGURATION_STATE, false);
+
+    }
 
 }
