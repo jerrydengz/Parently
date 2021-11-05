7fa79ff8aa351adaec697986fe78c3a8b41982b9
Jerry <zrd1@sfu.ca>
2021-11-04 22:28:27 -0700

    Implement ListView for displaying & Activities to configure children.

--- app/src/main/java/cmpt276/phosphorus/childapp/ChildrenActivity.java
+++ app/src/main/java/cmpt276/phosphorus/childapp/ChildrenActivity.java
@@ -5,17 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
+import java.util.List;
+
+import cmpt276.phosphorus.childapp.model.Child;
+import cmpt276.phosphorus.childapp.model.ChildManager;
+
 public class ChildrenActivity extends AppCompatActivity {
+
+    private ChildManager childManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_children);
 
+        childManager = ChildManager.getInstance();
         this.createBackBtn();
     }
+
 
     private void createBackBtn(){
         Button button = findViewById(R.id.btnBackChildren);
---
+++ app/src/main/java/cmpt276/phosphorus/childapp/ChildrenConfigureActivity.java
@@ -0,0 +1,16 @@
+package cmpt276.phosphorus.childapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChildrenConfigureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_children_configure);
+    }
+
+
+}
