79756986b228827002ff3e8b5a7042a71611d564
mattf <maf7@sfu.ca>
2021-11-12 11:47:20 -0800

    Added the Task class and added it to ChildManager
    Added the Task class with fields for the name of the task (String), a list of UUIDs to keep track of who is next for the task, and a String to easily get the name of the child who is next. Added methods to cycle UUIDs upon completion, remove a child that has been deleted, set the next child, and setters as needed. Added a list of tasks to ChildManager and implemented methods to get needed data and to delete children from tasks when they are deleted in allChildren

---
+++ app/src/main/java/cmpt276/phosphorus/childapp/model/Task.java
@@ -0,0 +1,61 @@
+package cmpt276.phosphorus.childapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Task {
+    private String name;
+    private final List<UUID> children;
+    private final int FIRST_INDEX = 0;
+    private String currentChildName;
+
+    public Task(String name, List<UUID> children, Child currentChild){
+        this.name = name;
+        this.children = new ArrayList<>();
+        this.children.add(currentChild.getUUID());
+        for(UUID iter : children){
+            if(iter != currentChild.getUUID()){
+                this.children.add(iter);
+            }
+        }
+        this.currentChildName = currentChild.getName();
+    }
+
+    protected String getName() {
+        return this.name;
+    }
+
+    protected void changeName(String newName){
+        this.name = newName;
+    }
+
+    protected void setNextChild(UUID nextChild){
+        this.children.remove(nextChild);
+        this.children.add(this.FIRST_INDEX, nextChild);
+    }
+
+    protected UUID getCurrentChild(){
+        return this.children.get(this.FIRST_INDEX);
+    }
+
+    protected void cycleChildren(){
+        this.children.add(this.children.remove(this.FIRST_INDEX));
+    }
+
+    protected void removeChild(UUID child){
+        this.children.remove(child);
+    }
+
+    protected UUID getChild(int pos){
+        return this.children.get(pos);
+    }
+
+    public String getChildName(){
+        return this.currentChildName;
+    }
+
+    protected void changeChildName(String newName){
+        this.currentChildName = newName;
+    }
+}
