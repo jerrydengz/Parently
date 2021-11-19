05e1f441bb8b2af192e055c8cd0520dcd93a63f7
Jerry <zrd1@sfu.ca>
2021-11-19 15:32:58 -0800

    Implement Custom List View of Tasks

--- app/src/main/java/cmpt276/phosphorus/childapp/task/TaskActivity.java
+++ app/src/main/java/cmpt276/phosphorus/childapp/task/TaskActivity.java
@@ -6,23 +6,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
 
 import cmpt276.phosphorus.childapp.R;
-import cmpt276.phosphorus.childapp.help.HelpActivity;
 import cmpt276.phosphorus.childapp.model.TaskManager;
+import cmpt276.phosphorus.childapp.task.utils.TaskListAdapter;
 
 public class TaskActivity extends AppCompatActivity {
-
     private TaskManager taskManager;
 
     /*
      TODO ( whoever is going to do this :^) )
-         2. Create complex list item
-            i. task name
-            ii. child name of who's turn it is
          3. Create a Custom AlertDialog displaying task info
             i. a dialog button option to cancel
             ii. a dialog button option to edit -> goes to ConfigureTaskActivity.java (possibly implement delete button in here)
@@ -32,7 +31,6 @@ public class TaskActivity extends AppCompatActivity {
                  - picture of child
             iv. button to indicate "finished"
          5. implement adding task in ConfigureTaskActivity.java
-         6. create list adapter (inner/sub class) for tasks
      */
 
     @Override
@@ -47,6 +45,7 @@ public class TaskActivity extends AppCompatActivity {
 
         this.createConfigureTaskBtn();
         this.setUpTaskDialog();
+        this.populateTaskListView();
     }
 
     // If user select the top left back button
@@ -56,12 +55,18 @@ public class TaskActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.populateTaskListView();
+    }
+
     public static Intent makeIntent(Context context) {
         return new Intent(context, TaskActivity.class);
     }
 
     private void createConfigureTaskBtn() {
-        Button button = findViewById(R.id.add_task_fab);
+        FloatingActionButton button = findViewById(R.id.add_task_fab);
 
         // NOTE: preliminary, model after methods in ChildActivity.java to configure children
         button.setOnClickListener(view -> startActivity(ConfigureTaskActivity.makeIntent(this)));
@@ -72,5 +77,10 @@ public class TaskActivity extends AppCompatActivity {
     private void setUpTaskDialog() {
     }
 
+    private void populateTaskListView() {
+        ListAdapter listAdapter = new TaskListAdapter(this, taskManager.getAllTasks());
+        ListView listView = findViewById(R.id.taskListView);
+        listView.setAdapter(listAdapter);
+    }
 
 }
