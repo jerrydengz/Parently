febc229d07537768a761e78d0628bc1d46b595dc
mattf <maf7@sfu.ca>
2021-12-02 16:29:00 -0800

    Created the layout for task history
    Created the layout for displaying the TaskHistory, modeled after the display of the tasks themselves. Started working on TaskHistoryListAdapter but no real progress has been made

--- app/src/main/java/cmpt276/phosphorus/childapp/task/utils/TaskHistoryListAdapter.java
+++ app/src/main/java/cmpt276/phosphorus/childapp/task/utils/TaskHistoryListAdapter.java
@@ -1,5 +1,14 @@
 package cmpt276.phosphorus.childapp.task.utils;
 
+import android.content.Context;
+
+import java.util.List;
+
+import cmpt276.phosphorus.childapp.model.task.TaskHistory;
+
 public class TaskHistoryListAdapter {
 
+    public TaskHistoryListAdapter(Context context, List<TaskHistory> taskHist){
+        super();
+    }
 }
